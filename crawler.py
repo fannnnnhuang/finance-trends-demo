ea24a399716bac2fadeab08e69364e0a38c29231
fannnnnhuang <key0988480343@gmail.com>
2025-06-01 22:28:25 +0800

    fix USD

--- crawler.py
+++ crawler.py
@@ -21,10 +21,10 @@ twse_data = twse_response.json()
 taiex = twse_data['data'][-1][1]  # 假設收盤價在此位置
 
 # 抓取匯率（台幣對美元）
-exchange_url = "https://api.exchangerate-api.com/v4/latest/TWD"
+exchange_url = "https://open.er-api.com/v6/latest/USD"
 exchange_response = requests.get(exchange_url)
 exchange_data = exchange_response.json()
-usd_rate = exchange_data['rates']['USD']
+usd_rate = exchange_data['rates']['TWD']
 
 # 抓取 Google 熱門搜尋趨勢
 pytrends = TrendReq(hl='zh-TW', tz=540)
